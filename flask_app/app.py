5ebb6c52a9d6c76b2f385118836c702f0801b00f
Prit Thombare <prit@Prits-MacBook-Air.local>
2025-01-22 22:11:59 +0530

    35 API

--- flask_app/app.py
+++ flask_app/app.py
@@ -19,10 +19,10 @@ CORS(app)  # Allow cross-origin requests for API access
 
 # Watsonx AI credentials setup
 credentials = Credentials(
-    url="https://eu-gb.ml.cloud.ibm.com",
-    api_key="U4EE4SOGSKX_3eKMPDY0wMR5l7gcw-WmsGz88EAwG8lF" #API key
+    url="https://us-south.ml.cloud.ibm.com", #############URL##############
+    api_key="U4EE4SOGSKX_3eKMPDY0wMR5l7gcw-WmsGz88EAwG8lF" ############API key#############
 )
-project_id = os.getenv("PROJECT_ID", "fa51d7ee-4ca0-473a-b741-45dd29e0b2e4") #Project ID
+project_id = os.getenv("PROJECT_ID", "fa51d7ee-4ca0-473a-b741-45dd29e0b2e4") ############Project ID#############
 
 # Initialize Watsonx Granite model
 model_id = ModelTypes.GRANITE_13B_CHAT_V2
